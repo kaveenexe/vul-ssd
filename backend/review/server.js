702b4f481ffb84d00cd529c1cf6b12172794588e
Kaveen Uththara <it21108990@my.sliit.lk>
2024-09-26 16:59:06 +0530

    nosql, csp, dos

--- backend/review/server.js
+++ backend/review/server.js
@@ -1,32 +1,47 @@
-// server.js
 require("dotenv").config();
 const express = require("express");
-
 const connectDB = require("./config/db");
+const helmet = require("helmet"); // Add helmet for security headers
 
 const app = express();
 
-const sellerReview = require("./routes/sellerReviewRoute"); // added
-const productReview = require("./routes/productReviewRoute"); // added
+const sellerReview = require("./routes/sellerReviewRoute"); 
+const productReview = require("./routes/productReviewRoute");
 
 var cors = require('cors')
 
 app.use(cors())
 
-// connect database
+// Connect database
 connectDB();
 
-// initialize middleware
+// Add Helmet to set security-related HTTP headers, including CSP
+app.use(
+    helmet({
+        contentSecurityPolicy: {
+            directives: {
+                defaultSrc: ["'self'"],
+                scriptSrc: ["'self'", "trusted-scripts.example.com"],
+                styleSrc: ["'self'", "https://trusted-styles.example.com"],
+                imgSrc: ["'self'", "data:"],
+                objectSrc: ["'none'"],
+                upgradeInsecureRequests: [],
+            },
+        },
+    })
+);
+
+// Initialize middleware
 app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("Server up and running"));
 
+app.use("/api/sellerReview", sellerReview); 
+app.use("/api/productReview", productReview);
 
-app.use("/api/sellerReview", sellerReview); // added
-app.use("/api/productReview", productReview); // added
-
-// setting up port
+// Set up port
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log(`server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
